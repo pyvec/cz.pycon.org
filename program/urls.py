e274e246aa32803b36123556048df32bc0208d95
Jan Smitka <jan.smitka@lynt.cz>
2023-08-11 09:42:59 +0200

    Fix redirect from `/2023/program/` to `/2023/program/talks/`

--- program/urls.py
+++ program/urls.py
@@ -6,7 +6,7 @@ from .views import preview, talks_list, workshops_list
 app_name = "program"
 
 urlpatterns = [
-    re_path("^$", RedirectView.as_view(pattern_name="talks_list")),
+    re_path("^$", RedirectView.as_view(pattern_name="program:talks_list")),
     re_path("^talks/$", talks_list, name="talks_list"),
     re_path("^workshops/$", workshops_list, name="workshops_list"),
     re_path("^preview/$", preview, name="preview"),
