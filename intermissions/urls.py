26688f9495606362048995fe82bdbd91d2c0b02f
Jan Smitka <jan.smitka@lynt.cz>
2023-09-12 16:40:59 +0200

    Use `path()` in urlpatterns for intermissions

--- intermissions/urls.py
+++ intermissions/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 
 from .views import announcements, index, slido, sponsors
 
 urlpatterns = [
-    re_path("^$", index, name="cycle_all"),
-    re_path("^sponsors/(?P<level>[\w\-]+)/$", sponsors, name="sponsors"),
-    re_path("^announcements/$", announcements, name="announcements"),
-    re_path("^slido/$", slido, name="slido"),
+    path("", index, name="cycle_all"),
+    path("sponsors/<level>/", sponsors, name="sponsors"),
+    path("announcements/", announcements, name="announcements"),
+    path("slido/", slido, name="slido"),
 ]
